aa840ce81254ed845ac43ecb56521d02803f79e2
dburnaye <dburnaye@gmu.edu>
2025-05-02 15:34:08 -0400

    Created assignment python file and started task 1

---
+++ assignment1.py
@@ -0,0 +1,9 @@
+# Assignment: Exploring Python Concepts, done by Daniel Burnayev
+
+# Task 1:
+name = "Daniel"
+age = 20
+height = 6.3
+
+print("Hey y'all, this is " + name + " here, " + age + " years old and " + height + " inches tall.")
+
