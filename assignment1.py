dc76c3ed7fc581466c19bad2978536ebb306f198
dburnaye <dburnaye@gmu.edu>
2025-05-02 15:52:17 -0400

    Pushong local directory state to repo

--- assignment1.py
+++
@@ -1,8 +0,0 @@
-# Assignment: Exploring Python Concepts, done by Daniel Burnayev
-
-# Task 1:
-name = "Daniel"
-age = 20
-height = 6.3
-
-print("Hey y'all, this is " + name + " here, " + str(age) + " years old and " + str(height) + " feet tall.")
