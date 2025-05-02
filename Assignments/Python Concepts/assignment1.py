caa487885fc4198683986c414dcfe0d9f92806dd
dburnaye <dburnaye@gmu.edu>
2025-05-02 15:39:19 -0400

    Establishing repo format and including assignment 1 directory

---
+++ Assignments/Python Concepts/assignment1.py
@@ -0,0 +1,8 @@
+# Assignment: Exploring Python Concepts, done by Daniel Burnayev
+
+# Task 1:
+name = "Daniel"
+age = 20
+height = 6.3
+
+print("Hey y'all, this is " + name + " here, " + str(age) + " years old and " + str(height) + " feet tall.")
