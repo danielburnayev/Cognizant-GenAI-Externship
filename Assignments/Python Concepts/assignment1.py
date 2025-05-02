fea8776e12da75e128e33b6896954c75959ecee3
dburnaye <dburnaye@gmu.edu>
2025-05-02 15:50:10 -0400

    Completed task 2

--- Assignments/Python Concepts/assignment1.py
+++ Assignments/Python Concepts/assignment1.py
@@ -6,3 +6,11 @@ age = 20
 height = 6.3
 
 print("Hey y'all, this is " + name + " here, " + str(age) + " years old and " + str(height) + " feet tall.")
+
+# Task 2:
+num1 = 9
+num2 = 10
+print("The sum of " + str(num1) + " and " + str(num2) + " is: ", num1 + num2, "\n")
+print("The difference of " + str(num1) + " and " + str(num2) + " is: ", num1 - num2, "\n")
+print("The product of " + str(num1) + " and " + str(num2) + " is: ", num1 * num2, "\n")
+print("The quotient of " + str(num1) + " and " + str(num2) + " is: ", num1 / num2, "\n")
