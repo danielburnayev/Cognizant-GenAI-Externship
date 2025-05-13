6cdb2341ca3b5ffb63d28a0881516eb8bb38a59d
dburnaye <dburnaye@gmu.edu>
2025-05-13 16:45:01 -0400

    Added responeses to negative numbers

--- Assignments/Loops in Python/assignment2.py
+++ Assignments/Loops in Python/assignment2.py
@@ -4,6 +4,9 @@ while count > 0:
     print(count, end=" ")
     count -= 1
 
-print("\nBlast off!")
+if count == 0:
+    print("\nBlast off!")
+else:
+    print("\nUmmmmm, blast off already happened. Maybe start from a postivie integer instead of a negative one")
 
 
