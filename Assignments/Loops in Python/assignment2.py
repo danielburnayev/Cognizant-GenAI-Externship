b72353e94db780206a32d745dc250cf7b0c5db70
dburnaye <dburnaye@gmu.edu>
2025-05-13 16:59:02 -0400

    Completed task 2

--- Assignments/Loops in Python/assignment2.py
+++ Assignments/Loops in Python/assignment2.py
@@ -10,3 +10,7 @@ else:
     print("\nUmmmmm, blast off already happened. Maybe start from a postivie integer instead of a negative one")
 
 
+# task 2
+multiplier = int(input("Give a number, any number: "))
+for multiplicand in range(1, 11): # multiplying from 1 to 10
+    print(multiplier, " X ", multiplicand, " = ", multiplier * multiplicand)
