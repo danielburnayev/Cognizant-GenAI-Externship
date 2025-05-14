d50b4198f665bc1fc13b35893923c6f5e22a2d77
dburnaye <dburnaye@gmu.edu>
2025-05-14 04:10:57 -0400

    Completed task 1

---
+++ Assignments/String Methods/assignment3.py
@@ -0,0 +1,10 @@
+# task 1
+init_string = "Python is amazing!"
+
+first_six = init_string[0:6]
+amazing_part = init_string[10:len(init_string) - 1]
+reverse_string = init_string[::-1]
+
+print("First 6 characters:", first_six + "\n", "Amazing part:", amazing_part + "\n", "Reversed string:", reverse_string)
+
+
