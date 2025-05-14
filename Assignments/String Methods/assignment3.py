48069bb5d941525a421b7290383f888af8fcdd75
dburnaye <dburnaye@gmu.edu>
2025-05-14 04:34:38 -0400

    Completed task 2

--- Assignments/String Methods/assignment3.py
+++ Assignments/String Methods/assignment3.py
@@ -8,3 +8,11 @@ reverse_string = init_string[::-1]
 print("First 6 characters:", first_six + "\n", "Amazing part:", amazing_part + "\n", "Reversed string:", reverse_string)
 
 
+# task 2
+og_str = " hello, python world! "
+
+print("\nOriginal string: \" hello, python world! \"\n\
+Stripped string: \"" + og_str.strip() + "\"\n\
+Capitalized string: \"" + og_str.capitalize() + "\"\n\
+Replaced string: \"" + og_str.replace("world", "universe") + "\"\n\
+Uppercase string: \"" + og_str.upper() + "\"")
