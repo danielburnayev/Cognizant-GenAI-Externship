37dca23b55b7a0e96b63400d4163f5205753abd5
dburnaye <dburnaye@gmu.edu>
2025-05-16 00:15:33 -0400

    Completed task 1

--- Assignments/Python Data Structures/assignment4.py
+++ Assignments/Python Data Structures/assignment4.py
@@ -1,1 +1,12 @@
 # task 1
+fav_fruits = ["orange", "mandarin", "banana", "mango", "pear"]
+print("Original list:", fav_fruits)
+
+fav_fruits.append("apple")
+print("After adding a fruit:", fav_fruits)
+
+fav_fruits.remove("mango")
+print("After removing a fruit:", fav_fruits)
+
+fav_fruits.reverse()
+print("Reversed list:", fav_fruits)
