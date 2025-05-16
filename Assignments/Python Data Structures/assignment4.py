962c70dfa218ae653b1954bb6f0dfec55ad9b6bc
dburnaye <dburnaye@gmu.edu>
2025-05-16 00:36:11 -0400

    Completed task 2

--- Assignments/Python Data Structures/assignment4.py
+++ Assignments/Python Data Structures/assignment4.py
@@ -10,3 +10,16 @@ print("After removing a fruit:", fav_fruits)
 
 fav_fruits.reverse()
 print("Reversed list:", fav_fruits)
+
+
+# task 2
+dict = {"name": "Daniel Burnayev", "age": "20", "city": "Somewhere in VA"}
+dict.update({"favorite color": "blue"})
+dict["city"] = "Fairfax"
+key_str = "Keys: "
+val_str = "Values: "
+for pair in dict.items():
+    follower = ", " if pair[0] != "favorite color" else ""
+    key_str += pair[0] + follower
+    val_str += pair[1] + follower
+print("\n" + key_str, "\n" + val_str)
