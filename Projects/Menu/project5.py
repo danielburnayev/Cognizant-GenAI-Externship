7f7d0daaa67002630f68529e44d78e9e49e84968
dburnaye <dburnaye@gmu.edu>
2025-05-17 02:11:17 -0400

    Started project and made prompt menu

---
+++ Projects/Menu/project5.py
@@ -0,0 +1,4 @@
+correct_responses = ["factorial", "fact", "fibonnaci", "fib", "fractal", "frac", "exit"]
+response = input("Welcome to the everything-math simulator. Choose between the following options:\n\
+   - Factorial (fact)\n   - Fibonacci (fib)\n   - Fractal (frac)\n   - Exit (exit)\n\
+Option: ")
