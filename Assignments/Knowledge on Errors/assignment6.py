8fecfaf83f9779916658817bd701914d289b8972
dburnaye <dburnaye@gmu.edu>
2025-05-21 03:27:26 -0400

    Completed task 2

--- Assignments/Knowledge on Errors/assignment6.py
+++ Assignments/Knowledge on Errors/assignment6.py
@@ -14,6 +14,56 @@ def task_one_func():
     divide_one_hundred_by_num()
 
 
+# task 2
+class CustomInvalidRangeError(Exception):
+    pass    
+
+def force_index_error():
+    nums = [1, 2, 3, 4, 5]
+    return nums[100]
+
+def force_key_error():
+    example_dict = {"a": 1, "b": 4, "c": 0, "d": 21}
+    return example_dict["z"]
+
+def force_type_error():
+    wrong_str = "wrong " + 1
+    return wrong_str
+
+def task_two_func():
+    while True:
+        try:
+            response = int(input("Welcome to the error inducer! Select the type of error to be forced by typing out one of the numbers below:\n\
+\t1. Index Error\n\
+\t2. Key Error\n\
+\t3. Type Error\n\
+\t4. Quit (Not an error, just exiting out of the program)\n\
+Input: "))
+            
+            if response == 1:
+                force_index_error()
+            if response == 2:
+                force_key_error()
+            if response == 3:
+                force_type_error()
+
+            if response < 1 or response > 4:
+                raise CustomInvalidRangeError()
+            
+        except IndexError: 
+            print("Here, an IndexError is caused because index 100 cannot be accessed in the 5-length list [1, 2, 3, 4, 5].\n")
+        except KeyError: 
+            print("Here, a KeyError is caused because key \"z\" cannot be accessed in dictionary {\"a\": 1, \"b\": 4, \"c\": 0, \"d\": 21}.\n")
+        except TypeError: 
+            print("Here, a TypeError is caused because string \"wrong\" and number 1 cannot be concatenated together.\n")
+        except ValueError:
+            print(f"While not one of the proposed errors, a ValueError happened here because the given input, {response}, was not a number as expected.\n")
+        except CustomInvalidRangeError:
+            print(f"While not one of the proposed errors, a CustomInvalidRangeError (one I made myself) happened because the given input, {response}, while a number, is not a number from 1 to 4.\n")
+        else:
+            break
+
+
 # ––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
 # ––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
 #       runner code
@@ -21,3 +71,4 @@ def task_one_func():
 # ––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
 if __name__ == "__main__":
     task_one_func()
+    task_two_func()
